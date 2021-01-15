fcc5a4b1af541ae40e6d0a31fa76c2a54ed84dcb
Supertinypeanut <1176032017@qq.com>
2021-01-15 23:00:53 +0800

    feat: 增加组件props类型定义

--- packages/jsx.ts
+++ packages/jsx.ts
@@ -5,18 +5,27 @@ declare global {
     interface IntrinsicElements {
       'ok-avatar': OkAvatarAttributes // 人员头像
       'ok-person': OkPersonAttributes // 人员信息
+      'ok-person-group': OkPersonGroupAttributes // 人员信息组
       'ok-person-detail': OkPersonDetailAttributes // 人员卡片
+      'ok-popper': OkPopoverAttributes // 弹层
     }
 
     interface OkAvatarAttributes {
       person: Person
     }
 
+    interface OkPersonGroupAttributes {
+      person: Array<Person>
+      size: 'large' | 'middle' | 'small'
+    }
     interface OkPersonAttributes {
       person: Person
       size: 'large' | 'middle' | 'small'
     }
 
+    interface OkPopoverAttributes {
+      zIndex: String
+    }
     interface OkPersonDetailAttributes {
       person: Person
     }
