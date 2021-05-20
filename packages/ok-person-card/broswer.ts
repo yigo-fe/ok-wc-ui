57819528078db60b12cc850f3589119397474ab1
fujing <907434913@qq.com>
2021-05-20 11:47:00 +0800

    chore: 修改飞书自带浏览器判断

--- packages/ok-person-card/broswer.ts
+++ packages/ok-person-card/broswer.ts
@@ -3,13 +3,13 @@
  * @Author: 付静
  * @Date: 2021-05-19 15:48:50
  * @LastEditors: 付静
- * @LastEditTime: 2021-05-19 15:59:10
+ * @LastEditTime: 2021-05-20 10:04:25
  * @FilePath: /packages/ok-person-card/broswer.ts
  */
 const isBroswerAllowed = () => {
   const uAgent = window.navigator.userAgent
   // 判断飞书、钉钉和企微自带浏览器
-  if (uAgent.match(/(WebPage\/sidebar-semi|DingTalk|MicroMessenger)/i)) {
+  if (uAgent.match(/(DingTalk|MicroMessenger|WebPage\/sidebar-semi| Lark)/i)) {
     return false
   } else {
     return true
