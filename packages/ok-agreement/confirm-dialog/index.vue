2dc3a012ad7d972ea969a713cca47b3dafacec93
Elvis <shuaiqy@hotmail.com>
2021-11-16 20:05:39 +0800

    feat: 补充国际化文案 --story=1004015 --user=戚雨

--- packages/ok-agreement/confirm-dialog/index.vue
+++ packages/ok-agreement/confirm-dialog/index.vue
@@ -7,17 +7,20 @@
             </div>
             <div class="body">
                 <slot name="body">
-                    {{companyName}}非常注重您的隐私保护，为切实保障您的隐私权，优化您的体验，{{companyName}}根据现行法规及政策，制定本《<a :href="PP?.url" target="_blank">{{PP?.name}}</a>》，本条款旨在详细阐述{{companyName}}将如何处理您的个人信息和隐私信息，并申明了{{companyName}}对保护隐私的承诺，了解这些内容，对于您行使个人权利及保护您的个人信息至关重要，{{companyName}}也将按照业界成熟的安全标准，采取相应的安全保护措施来保障您的个人信息。请您在使用{{companyName}}产品或服务前务必认真阅读、了解并同意本条款
+                    <!-- <span v-if="i18n.locale === 'zh-CN'"> -->
+                        {{companyName}}非常注重您的隐私保护，为切实保障您的隐私权，优化您的体验，{{companyName}}根据现行法规及政策，制定本《<a :href="PP?.url" target="_blank">{{PP?.name}}</a>》，本条款旨在详细阐述{{companyName}}将如何处理您的个人信息和隐私信息，并申明了{{companyName}}对保护隐私的承诺，了解这些内容，对于您行使个人权利及保护您的个人信息至关重要，{{companyName}}也将按照业界成熟的安全标准，采取相应的安全保护措施来保障您的个人信息。请您在使用{{companyName}}产品或服务前务必认真阅读、了解并同意本条款
+                    <!-- </span> -->
                 </slot>
             </div>
             <div class="footer">
-                <div class="getout button" @click="onChange('escape')">不同意并退出</div>
-                <div class="agree button" @click="onChange('agree')">同意</div>
+                <div class="getout button" @click="onChange('escape')">{{i18n.$t('control.confirmDialog.escape', '不同意并退出')}}</div>
+                <div class="agree button" @click="onChange('agree')">{{i18n.$t('control.confirmDialog.agree', '同意')}}</div>
             </div>
         </div>
     </div>
 </template>
 <script lang="ts">
+import { i18n } from '../../locales';
 import { defineComponent, PropType, ref, watch } from 'vue'
 import { getAgreement, AGREEMENTS_TYPE } from '../const'
 export default defineComponent({
@@ -47,6 +50,7 @@ export default defineComponent({
             onChange,
             inner_visible,
             PP,
+            i18n,
         }
     },
 })
--- packages/ok-agreement/const.ts
+++ packages/ok-agreement/const.ts
@@ -8,17 +8,23 @@ export const AGREEMENTS = [
     {
         key: AGREEMENTS_TYPE.SA,
         name: '产品服务协议',
+        enName: '',
         url: 'https://fe-resource.baiteda.com/services-statement/v0.1/services.html',
+        enUrl: ''
     },
     {
         key: AGREEMENTS_TYPE.PP,
         name: '隐私条款',
+        enName: 'Privacy Policy',
         url: 'https://fe-resource.baiteda.com/services-statement/v0.1/privacy.html',
+        enUrl: ''
     },
     {
         key: AGREEMENTS_TYPE.IPRs,
         name: '知识产权条款',
+        enName: '',
         url: 'https://fe-resource.baiteda.com/services-statement/v0.1/IPRs.html',
+        enUrl: ''
     }
 ]
 
